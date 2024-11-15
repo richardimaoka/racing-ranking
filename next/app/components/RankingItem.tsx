cc62f82e089b118947e3c9105df8982f865c0b08
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-16 05:58:08 +0900

    number formatter

--- next/app/components/RankingItem.tsx
+++ next/app/components/RankingItem.tsx
@@ -6,12 +6,16 @@ interface Props {
   interval?: number;
 }
 
+const intervalFormatter = new Intl.NumberFormat("en", {
+  minimumFractionDigits: 4,
+});
+
 export function RankingItem(props: Props) {
   return (
     <div className={styles.component}>
       <div>{props.ranking}</div>
       <div>{props.name}</div>
-      <div>{props.interval}</div>
+      <div>{props.interval && intervalFormatter.format(props.interval)}</div>
     </div>
   );
 }
