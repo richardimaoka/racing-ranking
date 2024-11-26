870ee1409a96f77450ade42d8029e42129ba1ce8
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-26 15:42:16 +0900

    animate height

--- next/app/components/ShrinkItem.tsx
+++ next/app/components/ShrinkItem.tsx
@@ -5,10 +5,14 @@ type Props = {
   children: ReactNode;
 };
 
+type AnimationPhase = "pre" | "ready" | "animating" | "done";
+
 export function ShrinkItem(props: Props) {
   const ref = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(0);
-  console.log("ShrinkItem", height);
+  const [phase, setPhase] = useState<AnimationPhase>("pre");
+
+  console.log("ShrinkItem", phase, height);
 
   useEffect(() => {
     if (ref.current) {
@@ -16,11 +20,46 @@ export function ShrinkItem(props: Props) {
     }
   }, []);
 
+  useEffect(() => {
+    if (phase === "pre" && height > 0) {
+      setPhase("ready");
+    } else if (phase === "ready") {
+      // without settimeout, the height doesn't animate but immediately becomes 0
+      const timeoutId = setTimeout(async () => {
+        setPhase("animating");
+      }, 10);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [phase, height]);
+
+  function transitionToDone() {
+    setPhase("done");
+  }
+
+  function calcStyle() {
+    switch (phase) {
+      case "pre":
+        return undefined;
+      case "ready":
+        return { height: height };
+      case "animating":
+        return { height: 0 };
+      case "done":
+        return { height: 0 };
+      default:
+        const _exhaustiveCheck: never = phase;
+        return _exhaustiveCheck;
+    }
+  }
+
   return (
     <div
       ref={ref}
-      style={height > 0 ? { height: 0 } : undefined}
+      style={calcStyle()}
       className={styles.component}
+      onTransitionEnd={transitionToDone}
     >
       {props.children}
     </div>
