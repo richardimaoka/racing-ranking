b42aed2730df651425a5890e8b3e1be4540b4a63
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-27 04:19:21 +0900

    switch in RetirementPanelState

--- next/app/components/RankingPanelState.tsx
+++ next/app/components/RankingPanelState.tsx
@@ -24,8 +24,11 @@ async function updateItems(count: number) {
   return nextItems;
 }
 
+type AnimationPhase = "fetch" | "retire"; //| "pit in";
+
 export function RankingPanelState(props: Props) {
   const [count, setCount] = useState(2); //2 = next count
+  const [phase, setPhase] = useState<AnimationPhase>("fetch");
   const [items] = useState(props.initialItems);
   const [nextItems, setNextItems] = useState([]);
 
@@ -43,23 +46,39 @@ export function RankingPanelState(props: Props) {
   // }, [count, items]);
 
   useEffect(() => {
-    if (count === 2) {
-      // Fetch the next data
-      const timeoutId = setTimeout(async () => {
-        const updatedItems = await updateItems(count);
-        setNextItems(updatedItems);
-        setCount(incrementCount(count));
-      }, 1000);
+    switch (phase) {
+      case "fetch":
+        if (count === 2) {
+          // Fetch the next data
+          const timeoutId = setTimeout(async () => {
+            const updatedItems = await updateItems(count);
+            setNextItems(updatedItems);
+            setCount(incrementCount(count));
+            setPhase("retire");
+          }, 1000);
 
-      return () => {
-        clearTimeout(timeoutId);
-      };
+          return () => {
+            clearTimeout(timeoutId);
+          };
+        }
+        break;
+      case "retire":
+        break;
+      default:
+        //https://www.typescriptlang.org/docs/handbook/2/narrowing.html#exhaustiveness-checking
+        const _exhaustiveCheck: never = phase;
+        return _exhaustiveCheck;
     }
-  }, [count, items]);
+  }, [count, items, phase]);
 
-  if (nextItems.length === 0) {
-    return <RankingPanelLayout items={items} />;
-  } else {
-    return <RankingRetirement currentItems={items} nextItems={nextItems} />;
+  switch (phase) {
+    case "fetch":
+      return <RankingPanelLayout items={items} />;
+    case "retire":
+      return <RankingRetirement currentItems={items} nextItems={nextItems} />;
+    default:
+      //https://www.typescriptlang.org/docs/handbook/2/narrowing.html#exhaustiveness-checking
+      const _exhaustiveCheck: never = phase;
+      return _exhaustiveCheck;
   }
 }
