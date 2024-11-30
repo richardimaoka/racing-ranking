097e3929a7a969d3846a7b797b772b9744242cc5
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-30 12:57:21 +0900

    cleanup removeitem

--- next/app/components/animation/RemoveItem.tsx
+++ next/app/components/animation/RemoveItem.tsx
@@ -19,8 +19,11 @@ export function RemoveItem(props: Props) {
   const [height, setHeight] = useState(0);
   const [phase, setPhase] = useState<AnimationPhase>("pre");
 
+  // Necessary useEffect, because ref is set only after the initial rendering.
   useEffect(() => {
     if (ref.current) {
+      // If you dare to move this block out of useEffect, and put direclty in the component (initialization) logic, the block will never run.
+      // This is because, at first, ref.current is undefined, and nothing triggers the second rendering
       setHeight(ref.current.getBoundingClientRect().height);
     }
   }, []);
@@ -28,15 +31,22 @@ export function RemoveItem(props: Props) {
   const onHeightCalculated = props.onHeightCalculated;
   const onAnimationDone = props.onAnimationDone;
 
+  // Necessary useEffect, to avoid the following error
+  // Error: Cannot update a component while rendering a different component.
+  //        To locate the bad setState() call inside this component, follow the stack trace as described in
+  //        https://react.dev/link/setstate-in-render
+  useEffect(() => {
+    if (phase === "pre" && height !== 0) {
+      if (onHeightCalculated) {
+        onHeightCalculated(height);
+      }
+      setPhase("ready");
+    }
+  }, [height, onHeightCalculated, phase]);
+
   useEffect(() => {
     switch (phase) {
       case "pre":
-        if (height > 0) {
-          if (onHeightCalculated) {
-            onHeightCalculated(height);
-          }
-          setPhase("ready");
-        }
         return;
       case "ready":
         // without setTimeout, the height doesn't animate but immediately becomes 0
